0341518e4fa5a743b4fa625d8aa99d8b2573ff7c
Ruslan Useinov <r.useinov23@gmail.com>
2022-09-26 10:30:19 -0400

    feat: button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import React, { Children, ReactNode } from 'react';
+import clsx from 'clsx';
+
+const Button: React.FC<{
+  className?: string;
+  onClick?: () => void;
+  children:ReactNode
+}> = ({ children, className, onClick }) => {
+  return (
+    <div
+      className={clsx(
+        'bg-white',
+        'hover:bg-gradient-to-tr hover:from-primary-500 hover:via-primary-400 hover:to-white',
+        'p-[2px] mr-2 my-2 rounded-md flex justify-center items-center',
+        'hover:border-primary-500 transition-all duration-200',
+        'scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu',
+        'transition duration-25',
+        'motion-reduce:hover:scale-100',
+        className
+      )}
+    >
+      <button
+        onClick={onClick}
+        className={clsx('px-5 py-1 rounded-md bg-dark')}
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
