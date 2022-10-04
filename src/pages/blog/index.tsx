fe2499c400d1450a25db9498216148b1f5608d87
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-04 17:46:24 -0400

    style: styles improvement

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -7,21 +7,12 @@ import Link from 'next/link';
 import Layout from '../../components/layout/Layout';
 import PostCard from '../../components/content/PostCard';
 import { allPosts, type Post } from 'contentlayer/generated';
+import Accent from 'src/components/Accent';
 
 export async function getStaticProps() {
-  const posts = allPosts
-    .filter((post) => new RegExp(/^(blog\/)/).exec(post._id))
-    .map((post) => {
-      return {
-        title: post.title,
-        publishedAt: post.publishedAt,
-        description: post.description,
-        cover_image: '',
-        slug: post.slug,
-        readingTime: post.readingTime
-      };
-    });
-
+  const posts = allPosts.filter((post) =>
+    new RegExp(/^(blog\/)/).exec(post._id)
+  );
   return {
     props: {
       posts: posts,
@@ -34,10 +25,13 @@ const BlogPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
 }) => {
   return (
     <Layout>
-      <main className="bg-dark text-white max-w-screen-lg m-auto p-2 min-h-screen h-full overflow-x-hidden ">
-        <h1 className="my-6">Blog</h1>
+      <main>
+      <h1 className="my-4">
+          <Accent>Blog</Accent>
+        </h1>
+        <div className="w-full h-1 my-4 bg-gray-300 rounded-lg" />
         <ul className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3">
-          {posts.map((postSummary: any) => {
+          {posts.map((postSummary) => {
             return (
               <Link
                 href={`/blog/${postSummary.slug}`}
