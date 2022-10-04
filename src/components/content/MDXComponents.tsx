24dc25830742f9be3f8fb644d936480951b90492
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-04 14:21:51 -0400

    refactor: html tag styles removed

--- src/components/content/MDXComponents.tsx
+++ src/components/content/MDXComponents.tsx
@@ -7,19 +7,11 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import atomOneDark from 'react-syntax-highlighter/dist/cjs/styles/prism/material-dark';
 
 const MDXComponents = {
-  // TestComponent: dynamic(() => import('../../components/TestComponent')),
   Image,
   Head,
-  h1: (props: any) => <h1 className="text-white my-3">{props.children}</h1>,
-  h2: (props: any) => <h2 className="text-white my-3">{props.children}</h2>,
-  h3: (props: any) => <h3 className="text-white my-3">{props.children}</h3>,
-  p: (props: any) => <p className="text-white my-2">{props.children}</p>,
-  code: (props: any) => highlighter(props),
 };
 
 export default MDXComponents;
-
-
 
 function highlighter({
   className,
