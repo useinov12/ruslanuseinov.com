886627b302033c97cecc4efd7c3757101e754c32
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-05 08:05:48 -0400

    style: minro style change

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,12 +4,12 @@ import clsx from 'clsx';
 const Button: React.FC<{
   className?: string;
   onClick?: () => void;
-  children:ReactNode
+  children: ReactNode;
 }> = ({ children, className, onClick }) => {
   return (
     <div
       className={clsx(
-        'bg-white', 
+        'bg-white',
         'hover:bg-gradient-to-tr hover:from-primary-500 hover:via-primary-500 hover:to-primary-400',
         'p-[2px] mr-2 my-2 rounded-md flex justify-center items-center',
         'hover:border-primary-500 transition-all duration-200',
