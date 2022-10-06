64980a072f93b1c64888e02e942231925b29cf54
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-06 14:12:15 -0400

    feat: mdx link with target blank

--- src/components/content/MDXComponents.tsx
+++ src/components/content/MDXComponents.tsx
@@ -1,37 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import dynamic from 'next/dynamic';
-import clsx from 'clsx';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import atomOneDark from 'react-syntax-highlighter/dist/cjs/styles/prism/material-dark';
+import Footer from '../layout/Footer';
+import UnstyledLink from '../UnstyledLink';
 
 const MDXComponents = {
   Image,
   Head,
+  Footer,
+  a:(props:any)=> <a href={props.href} target='_blank'>{props.children}</a>
 };
 
 export default MDXComponents;
-
-function highlighter({
-  className,
-  children,
-}: {
-  className: string;
-  children: any;
-}) {
-  const language = className?.includes('language')
-    ? className.replace('language-', '')
-    : '';
-
-  return (
-    <SyntaxHighlighter
-      className={clsx('rounded-lg py-4')}
-      PreTag="div"
-      language={language}
-      style={atomOneDark}
-    >
-      {children}
-    </SyntaxHighlighter>
-  );
-}
