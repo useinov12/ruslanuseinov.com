272fc10edb88b84711832d7db94855cebc7e5460
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-06 14:27:24 -0400

    style: minor style imprrovements

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import React, { Children, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import clsx from 'clsx';
 
 const Button: React.FC<{
--- src/components/TableOfContent.tsx
+++ src/components/TableOfContent.tsx
@@ -1,16 +1,16 @@
 import clsx from 'clsx';
 import React from 'react';
 import GithubSlugger from 'github-slugger';
-import { allPosts, type Post } from 'contentlayer/generated';
+import {type Post } from 'contentlayer/generated';
 
 const TableOfContents: React.FC<{ post: Post }> = ({ post }) => {
   const headingLines = post.body.raw
     .split('\n')
-    .filter((line: any) => line.match(/^###*\s/));
+    .filter((line: string) => line.match(/^#*\s/));
 
   const headings = headingLines.map( (raw: any) => {
-    const text = raw.replace(/^###*\s/, '').trim();
-    const level = raw.slice(0, 3) === '###' ? 3 : 2;
+    const text = raw.replace(/^#*\s/, '').trim();
+    const level = raw.slice(0, 3) === '#' ? 3 : 2;
     const slugger = new GithubSlugger();
 
     return {
@@ -31,7 +31,7 @@ const TableOfContents: React.FC<{ post: Post }> = ({ post }) => {
             key={index}
             type="button"
             className={clsx(
-              `${heading.id === activeId ? 'text-primary-400' : 'text-gray-300'}`,
+              `${heading.id === activeId ? 'text-primary-500' : 'text-gray-300'}`,
               `${heading.level === 2 ? '' : 'hidden'} `,
               `mb-3 sm:text-md md:text-lg`,
               'whitespace-nowrap'
