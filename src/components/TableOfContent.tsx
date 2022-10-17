275bf84793d4a8e58eab973a0198977e1677c26c
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-17 15:19:35 -0400

    fix: code tags removed from headings search

--- src/components/TableOfContent.tsx
+++ src/components/TableOfContent.tsx
@@ -1,15 +1,16 @@
 import clsx from 'clsx';
 import React from 'react';
 import GithubSlugger from 'github-slugger';
-import {type Post } from 'contentlayer/generated';
+import { type Post } from 'contentlayer/generated';
 
 const TableOfContents: React.FC<{ post: Post }> = ({ post }) => {
   const headingLines = post.body.raw
     .split('\n')
-    .filter((line: string) => line.match(/^#*\s/));
+    .filter((line: string) => line.match(/^##\s/));
 
-  const headings = headingLines.map( (raw: any) => {
-    const text = raw.replace(/^#*\s/, '').trim();
+  const headings = headingLines.map((raw: any) => {
+    console.log(raw)
+    const text = raw.replace(/^##\s/, '').trim();
     const level = raw.slice(0, 3) === '#' ? 3 : 2;
     const slugger = new GithubSlugger();
 
@@ -24,26 +25,28 @@ const TableOfContents: React.FC<{ post: Post }> = ({ post }) => {
 
   return (
     <div className=" flex flex-col items-start mt-4 mb-0 ml-4 cursor-pointer ">
-      <h3 className='mb-4 font-mono text-xl'>Table of Content</h3>
+      <h3 className="mb-4 font-mono text-xl">Table of Content</h3>
       {headings.map((heading, index) => {
         return (
           <button
             key={index}
             type="button"
             className={clsx(
-              `${heading.id === activeId ? 'text-primary-500' : 'text-gray-300'}`,
+              `${
+                heading.id === activeId ? 'text-primary-500' : 'text-gray-300'
+              }`,
               `${heading.level === 2 ? '' : 'hidden'} `,
               `mb-3 sm:text-md md:text-lg`,
               'whitespace-nowrap'
             )}
             onClick={(e) => {
               e.preventDefault();
-                document.querySelector(`#${heading.id}`)?.scrollIntoView({
-                  behavior: 'smooth',
-                  block: 'start',
-                  inline: 'nearest',
-                });
-                setActiveId(heading.id)
+              document.querySelector(`#${heading.id}`)?.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+                inline: 'nearest',
+              });
+              setActiveId(heading.id);
             }}
           >
             {heading.text}
