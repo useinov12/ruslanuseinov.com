a6b148d190f86f8ef6139d55eedb8dc94fd8de29
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-19 11:42:48 -0400

    refactor: component moved to pages folder

---
+++ src/pages/blog.tsx
@@ -0,0 +1,60 @@
+import {
+  type GetStaticProps,
+  type InferGetStaticPropsType,
+  NextPage,
+} from 'next';
+import Link from 'next/link';
+import Layout from 'src/components/layout/Layout';
+import PostCard from 'src/components/content/PostCard';
+import { allPosts, type Post } from 'contentlayer/generated';
+import Accent from 'src/components/Accent';
+import clsx from 'clsx';
+import useLoaded from 'src/hooks/useLoaded';
+export async function getStaticProps() {
+  const posts = allPosts.filter((post) =>
+    new RegExp(/^(blog\/)/).exec(post._id)
+  );
+  return {
+    props: {
+      posts: posts,
+    },
+  };
+}
+
+const BlogPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  posts,
+}) => {
+  const isLoaded = useLoaded();
+  return (
+    <Layout>
+      <main className={clsx(isLoaded && 'fade-in-start')}>
+        <h1 className="my-4" data-fade="1">
+          <Accent>Blog</Accent>
+        </h1>
+        <h6 className="font-semibold text-xl text-gray-300" data-fade="2">
+          For patterns, tutorials and my setups
+        </h6>
+        <div
+          className="w-full h-[1px] mb-8 mt-4 bg-gray-300 rounded-lg"
+          data-fade="3"
+        />
+        <ul className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3" data-fade="4">
+          {posts.map((postSummary) => {
+            return (
+              <Link
+                href={`/blog/${postSummary.slug}`}
+                passHref
+                key={postSummary.slug}
+              >
+                <a>
+                  <PostCard postSummary={postSummary} />
+                </a>
+              </Link>
+            );
+          })}
+        </ul>
+      </main>
+    </Layout>
+  );
+};
+export default BlogPage;
---
+++ src/pages/library.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  type GetStaticProps,
+  type InferGetStaticPropsType,
+  NextPage,
+} from 'next';
+import Layout from 'src/components/layout/Layout';
+import Link from 'next/link';
+import PostCard from 'src/components/content/PostCard';
+import Accent from 'src/components/Accent';
+
+import { allPosts, type Post } from 'contentlayer/generated';
+import clsx from 'clsx'
+import useLoaded from 'src/hooks/useLoaded';
+
+export async function getStaticProps() {
+  const posts = allPosts.filter((post) =>
+    new RegExp(/^(library\/)/).exec(post._id)
+  );
+  return {
+    props: {
+      posts: posts,
+    },
+  };
+}
+
+
+
+const LibraryPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  posts,
+}) => {
+  const isLoaded = useLoaded();
+  return (
+    <Layout>
+      <main
+        className={clsx(
+          isLoaded && 'fade-in-start'
+        )}
+      >
+        <h1 className="my-4" data-fade="1">
+          <Accent>Library</Accent>
+        </h1>
+        <h6 className="font-semibold text-xl text-gray-300" data-fade="2">
+          Collection of code snippets
+        </h6>
+        <div className="w-full h-[1px] mb-8 mt-4 bg-gray-300 rounded-lg" data-fade="3"/>
+        <ul className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3" data-fade="4">
+          {posts.map((postSummary) => {
+            return (
+              <Link
+                href={`/library/${postSummary.slug}`}
+                passHref
+                key={postSummary.slug}
+              >
+                <a>
+                  <PostCard postSummary={postSummary} />
+                </a>
+              </Link>
+            );
+          })}
+        </ul>
+      </main>
+    </Layout>
+  );
+};
+
+export default LibraryPage;
