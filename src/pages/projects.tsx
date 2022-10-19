736ae82626a3b68624f504ef51769d6b638ee066
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-19 11:48:15 -0400

    refactor: component moved to pages folder

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
 import Link from 'next/link';
-import dayjs from 'dayjs';
 import clsx from 'clsx';
 
 import Accent from '../components/Accent';
