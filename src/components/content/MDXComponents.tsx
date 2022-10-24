bdfdc9d210e576ca6ee1aa4a0b9914289e114634
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-24 17:05:16 -0400

    feat: tech icons for mdx

--- src/components/content/MDXComponents.tsx
+++ src/components/content/MDXComponents.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Footer from '../layout/Footer';
-import UnstyledLink from '../UnstyledLink';
+import TechIcons from './TechIcons';
 
 const MDXComponents = {
   Image,
   Head,
   Footer,
-  a:(props:any)=> <a href={props.href} target='_blank' rel="noreferrer" >{props.children}</a>
+  a:(props:any)=> <a href={props.href} target='_blank' rel="noreferrer" >{props.children}</a>,
+  TechIcons
 };
 
 export default MDXComponents;
---
+++ src/components/content/TechIcons.tsx
@@ -0,0 +1,133 @@
+import clsx from 'clsx';
+import * as React from 'react';
+import { IoLogoVercel } from 'react-icons/io5';
+import {
+  SiFirebase,
+  SiGit,
+  SiGoogleanalytics,
+  SiJavascript,
+  SiMarkdown,
+  SiMongodb,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiNotion,
+  SiPrettier,
+  SiReact,
+  SiRedux,
+  SiSass,
+  SiTailwindcss,
+  SiTypescript,
+} from 'react-icons/si';
+import contentlayerimg from '../../../public/assets/contentlayerimg.png';
+import Image from 'next/image';
+
+import Tooltip from '../Tooltip';
+
+export type TechListType = keyof typeof techList;
+
+export type TechIconsProps = {
+  techs: Array<TechListType>;
+} & React.ComponentPropsWithoutRef<'ul'>;
+
+const TechIcons = ({ className, techs }: TechIconsProps) => {
+  return (
+    <ul className={clsx(className, 'flex gap-2')}>
+      {techs.map((tech) => {
+        if (!techList[tech]) return;
+
+        const current = techList[tech];
+
+        return (
+          <Tooltip key={current.name} content={<p>{current.name}</p>}>
+            <li
+              className={clsx(
+                // 'h-20 w-20',
+                'text-gray-300 hover:text-primary-500 dark:text-gray-200 dark:hover:text-primary-300',
+                'transition-colors'
+              )}
+            >
+              {current.name === 'ContentLayer' ? (
+                <Image src={contentlayerimg} height={35} width={35} />
+              ) : (
+                <current.icon />
+              )}
+            </li>
+          </Tooltip>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default TechIcons;
+
+const techList = {
+  contentlayer: {
+    icon: '',
+    name: 'ContentLayer',
+  },
+  react: {
+    icon: SiReact,
+    name: 'React',
+  },
+  nextjs: {
+    icon: SiNextdotjs,
+    name: 'Next.js',
+  },
+  tailwindcss: {
+    icon: SiTailwindcss,
+    name: 'Tailwind CSS',
+  },
+  scss: {
+    icon: SiSass,
+    name: 'SCSS',
+  },
+  javascript: {
+    icon: SiJavascript,
+    name: 'JavaScript',
+  },
+  typescript: {
+    icon: SiTypescript,
+    name: 'TypeScript',
+  },
+  nodejs: {
+    icon: SiNodedotjs,
+    name: 'Node.js',
+  },
+  firebase: {
+    icon: SiFirebase,
+    name: 'Firebase',
+  },
+  mongodb: {
+    icon: SiMongodb,
+    name: 'MongoDB',
+  },
+  swr: {
+    icon: IoLogoVercel,
+    name: 'SWR',
+  },
+  redux: {
+    icon: SiRedux,
+    name: 'Redux',
+  },
+  mdx: {
+    icon: SiMarkdown,
+    name: 'MDX',
+  },
+  prettier: {
+    icon: SiPrettier,
+    name: 'Prettier',
+  },
+  analytics: {
+    icon: SiGoogleanalytics,
+    name: 'Google Analytics',
+  },
+  git: {
+    icon: SiGit,
+    name: 'Git',
+  },
+  notion: {
+    icon: SiNotion,
+    name: 'Notion API',
+  },
+};
