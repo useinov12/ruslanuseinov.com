2d624ff862d81369eb0126e62c7972ebe62607e7
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-25 10:09:51 -0400

    fix: of image link

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -33,29 +33,29 @@ export default function Seo(props: SeoProps) {
 
   return (
     <Head>
-        <title>{meta.title}</title>
-        <meta name="robots" content={meta.robots} />
-        <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`${meta.url}${router.asPath}`} />
-        {/* <meta property="og:image" content={`${meta.url}/favicon/og.png`} /> */}
-        <link rel="canonical" href={`${meta.url}${router.asPath}`} />
+      <title>{meta.title}</title>
+      <meta name="robots" content={meta.robots} />
+      <meta content={meta.description} name="description" />
+      <meta property="og:url" content={`${meta.url}${router.asPath}`} />
+      {/* <meta property="og:image" content={`${meta.url}/favicon/og.png`} /> */}
+      <link rel="canonical" href={`${meta.url}${router.asPath}`} />
 
-        {/* Open Graph */}
-        <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content={meta.siteName} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta name="image" property="og:image" content={`${meta.url}/favicon/og.png`} />
+      {/* Open Graph */}
+      <meta property="og:type" content={meta.type} />
+      <meta property="og:site_name" content={meta.siteName} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:title" content={meta.title} />
+      <meta name="image" property="og:image" content={`/favicon/og.png`} />
 
-        {/* Favicons */}
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
-        <link rel="manifest" href="favicon/site.webmanifest" />
-        <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
+      {/* Favicons */}
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+      <link rel="manifest" href="favicon/site.webmanifest" />
+      <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 }
