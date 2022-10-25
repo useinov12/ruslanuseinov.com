01627a5d9348457ca64db978b7b3264250133ad4
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-25 12:09:21 -0400

    chore: next seo config

---
+++ next-seo.config.js
@@ -0,0 +1,22 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://ruslan-useinov.com',
+    siteName: 'ruslan-useinov.com',
+    images: [
+      {
+        url: 'https://ruslan-useinov.com/favicon/og.jpg',
+        width: 1200,
+        height: 900,
+        alt: 'Og Image Alt',
+        type: 'image/jpeg',
+      },
+    ],
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+};
