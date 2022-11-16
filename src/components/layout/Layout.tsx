0880f932e979553ad0af2e16c7dc2deb40baa929
Ruslan Useinov <r.useinov23@gmail.com>
2022-11-16 11:25:39 -0500

    feat:  theme context

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,28 +5,29 @@ import Header from './Header';
 import Footer from './Footer';
 
 import { PreloadProvider } from 'src/context/PreloadContext';
+import { ThemeContext } from 'src/context/ThemeProvider';
 
 const Layout: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
+  const { theme } = React.useContext(ThemeContext);
+
   return (
     <>
-      <PreloadProvider>
-        <Header />
-        <div
-          className={clsx(
-            'relatve',
-            'max-w-screen-lg',
-            'm-auto h-full',
-            'min-h-[90vh]',
-            'bg-dark text-white',
-            'px-3 lg:px-0'
-          )}
-        >
-          {children}
-        </div>
-        <Footer />
-      </PreloadProvider>
+      <div
+        className={clsx(
+          'w-screen h-full',
+          theme === 'light' ? 'bg-gray-300 text-dark' : 'bg-dark text-white'
+        )}
+      >
+        {/* <PreloadProvider> */}
+          <Header />
+          <div className={clsx('mx-auto max-w-screen-lg')}>
+            <div className="min-h-[90vh]">{children}</div>
+            <Footer />
+          </div>
+        {/* </PreloadProvider> */}
+      </div>
     </>
   );
 };
