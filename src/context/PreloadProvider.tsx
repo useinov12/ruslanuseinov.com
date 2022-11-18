718685e278e922f6b75bb8e7cdbe412cfc537000
Ruslan Useinov <r.useinov23@gmail.com>
2022-11-18 09:22:09 -0500

    refactor: renamed and dom event listener added

--- src/context/PreloadProvider.tsx
+++ src/context/PreloadProvider.tsx
@@ -3,25 +3,23 @@ import * as React from 'react';
 
 const PreloadContext = React.createContext<boolean>(false);
 
-export function PreloadProvider({ children }: { children: React.ReactNode }) {
+export default  function PreloadProvider({ children }: { children: React.ReactNode }) {
   /** If the dom is loaded */
   const [preloaded, setIsPreloaded] = React.useState<boolean>(false);
 
   React.useEffect(() => {
-    setTimeout(() => {
+    // setTimeout(() => {
+      
+    // }, 100);
+
+    document.addEventListener('DOMContentLoaded', (event) => {
       setIsPreloaded(true);
-    }, 100);
+    });
 
   }, []);
 
   return (
     <PreloadContext.Provider value={preloaded}>
-      <div
-        className={clsx(
-          'fixed inset-0 flex items-center justify-center transition-opacity bg-dark',
-          preloaded && 'pointer-events-none opacity-0'
-        )}
-      />
       {children}
     </PreloadContext.Provider>
   );
--- src/hooks/useLoaded.tsx
+++ src/hooks/useLoaded.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { usePreloadState } from 'src/context/PreloadContext';
+import { usePreloadState } from 'src/context/PreloadProvider';
 
 export default function useLoaded() {
   const preloaded = usePreloadState();
