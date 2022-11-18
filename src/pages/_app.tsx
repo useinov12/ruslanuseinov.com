92bc446d965754e2a1b52ac58f0295fe5b8f2141
Ruslan Useinov <r.useinov23@gmail.com>
2022-11-18 09:24:05 -0500

    feat: preload context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,13 +12,16 @@ import '../../next-seo.config';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../../next-seo.config';
 import ThemeProvider from 'src/context/ThemeProvider';
+import PreloadProvider from 'src/context/PreloadProvider';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider>
-      <DefaultSeo {...SEO}/>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <PreloadProvider>
+      <ThemeProvider>
+        <DefaultSeo {...SEO}/>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </PreloadProvider>
   );
 };
 
