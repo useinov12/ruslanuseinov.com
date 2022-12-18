60b5a144a3d3b34dc6b6ffbe84df9a32e2167f62
Ruslan Useinov <r.useinov23@gmail.com>
2022-12-18 09:31:29 -0500

    style: image and text align

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
 import Image from 'next/image';
-import me from '../../public/assets/me-sq.png';
 import clsx from 'clsx';
 import useLoaded from 'src/hooks/useLoaded';
 import TechStack from 'src/components/TechStack';
@@ -40,21 +39,13 @@ const AboutPage = () => {
           <div className="py-2 md:py-0">
             <h1 className="text-4xl font-bold">Ruslan Useinov</h1>
             <h4 className="mb-1">Front end developer</h4>
-            <p className="text-gray-300">
-              Creating beautiful and performant web
-            </p>
+            <p className="">Creating beautiful and performant web</p>
           </div>
-          <div
-            className={clsx(
-              'w-[100px] h-[100px] md:w-[150px] md:h-[150px] relative rounded-full',
-              'border-4',
-              theme === 'light' ? 'border-gray-800' : 'border-gray-50'
-            )}
-          >
+          <div className="w-32 h-32 relative rounded-full">
             <Image
-              src={me}
-              layout={'fill'}
-              objectFit={'contain'}
+              src={'/assets/me-sq.jpg'}
+              width="110"
+              height="110"
               className="rounded-full"
             />
           </div>
@@ -64,30 +55,30 @@ const AboutPage = () => {
           className="flex flex-col-reverse md:flex-row justify-between "
           data-fade="4"
         >
-          <section className="py-1 font-normal text-lg md:flex md:flex-col md:items-center tracking-tight">
-            <p className="py-2  md:w-3/4">
+          <section className="py-1 font-normal text-lg md:flex md:flex-col md:items-start tracking-tight">
+            <p className="py-2">
               Hi! I&apos;m Ruslan. I started learning web development in 2021.
             </p>
-            <p className="py-2 md:w-3/4">
+            <p className="py-2">
               I have always been interested in how the Internet works, but I
               never considered myself a tech person enough to figure things out.
               Until I actually started reading and practicing with Javascript!
             </p>
-            <p className="py-2 md:w-3/4">
+            <p className="py-2">
               At the time I start realizing that I want to have a career, but I
               didn&apos;t have any idea in what field.
             </p>
-            <p className="py-2 md:w-3/4">
+            <p className="py-2">
               And my practice with Javascript led me to dive deeper into web
               development and front end. I love the visual aspects of the web
               and I&apos;m motivated to learn as much as possible.
             </p>
-            <p className=" py-2 md:w-3/4">
+            <p className=" py-2">
               Right now, I want to master my skills and be as fluent as possible
               with modern frameworks and then invest more time in UI to learn
               how to design interfaces quick, effective, and scalable.
             </p>
-            <p className=" py-3 w-3/4">
+            <p className=" p-2">
               Here I will be writing some notes and showcasing what I do. I
               believe that documenting is the best way to learn things, and I
               can share my knowledge along the way.
