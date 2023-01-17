088c29b360860197d3e2fb3afaaa9808d1417aef
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-17 18:27:15 -0500

    refactor: unused components removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import {
-  type GetStaticProps,
-  type InferGetStaticPropsType,
-  NextPage,
-} from 'next';
 import Layout from '../components/layout/Layout';
 import clsx from 'clsx';
 import Accent from '../components/Accent';
@@ -16,33 +11,8 @@ import Link from 'next/link';
 import useLoaded from 'src/hooks/useLoaded';
 import { NextSeo } from 'next-seo';
 import { ThemeContext } from 'src/context/ThemeProvider';
-import { allPosts, type Post } from 'contentlayer/generated';
-import PostCard from 'src/components/content/PostCard';
 
-export async function getStaticProps() {
-  const posts = allPosts.filter((post) =>
-    new RegExp(/^(blog\/)/).exec(post._id)
-  );
-  const projects = allPosts.filter((post) =>
-    new RegExp(/^(projects\/)/).exec(post._id)
-  );
-  const snippets = allPosts.filter((post) =>
-    new RegExp(/^(library\/)/).exec(post._id)
-  );
-  return {
-    props: {
-      posts: posts,
-      projects:projects,
-      snippets:snippets
-    },
-  };
-}
-
-const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
-  posts,
-  projects,
-  snippets,
-}) => {
+export default function HomePage() {
   const isLoaded = useLoaded();
   const { theme } = React.useContext(ThemeContext);
   return (
@@ -53,7 +23,6 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
           'max-w-screen-lg',
           'flex flex-col justify-center',
           'w-full h-full',
-          // 'relative',
           isLoaded && 'fade-in-start'
         )}
       >
@@ -116,111 +85,10 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
             <Polkadots className="hidden md:block md:absolute -bottom-36 right-0 -z-10 w-2/5" />
           </div>
         </section>
-        {/* <BlogPosts posts={posts}/>
-        <Projects projects={projects}/>
-        <Library snippets={snippets}/> */}
       </main>
     </Layout>
   );
-};
-
-export default Home;
-
-const BlogPosts: NextPage<{posts:Post[]}> = ({
-  posts,
-}) => {
-  return (
-    <section className="p-2 lg:p-0 h-[80vh] flex flex-col relative my-8">
-      <h2 className="text-3xl md:text-5xl tracking-tight font-extrabold">
-        Featured Posts
-      </h2>
-      <ul
-        className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3 my-8"
-        data-fade="4"
-      >
-        {posts.map((postSummary) => {
-          return (
-            <Link
-              href={`/blog/${postSummary.slug}`}
-              passHref
-              key={postSummary.slug}
-            >
-              <a>
-                <PostCard postSummary={postSummary} />
-              </a>
-            </Link>
-          );
-        })}
-      </ul>
-
-      <Polkadots className="absolute bottom-10 left-0 w-2/5" />
-    </section>
-  );
-};
-
-const Projects: NextPage<{projects:Post[]}> = ({
-  projects,
-}) => {
-  return (
-    <section className="p-2 lg:p-0 h-[80vh] flex flex-col relative my-8">
-      <h2 className="text-3xl md:text-5xl tracking-tight font-extrabold">
-        Featured Projects
-      </h2>
-      <ul
-        className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3 my-8 z-10"
-        data-fade="4"
-      >
-        {projects.map((postSummary) => {
-          return (
-            <Link
-              href={`/blog/${postSummary.slug}`}
-              passHref
-              key={postSummary.slug}
-            >
-              <a>
-                <PostCard postSummary={postSummary} />
-              </a>
-            </Link>
-          );
-        })}
-      </ul>
-
-      <Polkadots className="absolute bottom-10 right-0 w-2/5 z-0" />
-    </section>
-  );
-};
-
-const Library: NextPage<{snippets:Post[]}> = ({
-  snippets,
-}) => {
-  return (
-    <section className="p-2 lg:p-0 h-[80vh] flex flex-col relative my-8">
-      <h2 className="text-3xl md:text-5xl tracking-tight font-extrabold">
-        Featured Code Snippets
-      </h2>
-      <ul
-        className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3 my-8"
-        data-fade="4"
-      >
-        {snippets.map((postSummary) => {
-          return (
-            <Link
-              href={`/blog/${postSummary.slug}`}
-              passHref
-              key={postSummary.slug}
-            >
-              <a>
-                <PostCard postSummary={postSummary} />
-              </a>
-            </Link>
-          );
-        })}
-      </ul>
-
-      <Polkadots className="absolute bottom-20 left-0 w-2/5" />
-    </section>
-  );
-};
+}
 
 const Polkadots = ({ className }: { className?: string }) => {
   return (
@@ -265,7 +133,7 @@ const linkList = [
   },
   {
     path: 'https://github.com/useinov12',
-    text: 'github.com/useinov12',
+    text: 'Github',
     Icon: SiGithub,
   },
 ];
