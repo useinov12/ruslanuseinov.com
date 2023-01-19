50ca14975ab11974ffca3dbeec6ec81d3e653a33
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-19 13:09:50 -0500

    refactor: post content layout

--- src/components/content/PostContent.tsx
+++ src/components/content/PostContent.tsx
@@ -15,8 +15,48 @@ const PostContent: FC<{
   const { theme } = useTheme();
   return (
     <main>
-      <h1 className="text-3xl my-2">{post.title}</h1>
-      <div className="inline-flex items-center gap-8 my-2">
+      <section
+        className={clsx(
+          'flex gap-6',
+          'md:flex-row',
+          'flex-col-reverse justify-between'
+        )}
+      >
+        <aside className="md:w-3/12">
+          <TableOfContents post={post} />
+        </aside>
+        <div className="flex flex-col md:w-9/12">
+          <article
+            className={clsx(
+              'h-[90vh] overflow-y-scroll',
+              'prose-sm',
+              'pt-3 pb-8 pr-2',
+              'prose-blockquote:border-4 prose-blockquote:border-transparent',
+              'prose-blockquote:border-l-primary-500 prose-blockquote:py-0',
+              'prose-code:text-primary-500',
+              // 'prose-code:text-sm',
+              'prose-li:list-disc',
+              'prose-a:text-primary-500',
+              theme === 'light' && 'text-dark'
+            )}
+          >
+            <ArticleHeader post={post} />
+            <MDXContent components={MDXComponents} />
+          </article>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default PostContent;
+
+function ArticleHeader({ post }: { post: any }) {
+  const { theme } = useTheme();
+  return (
+    <>
+      <h4 className="text-2xl my-1">{post.title}</h4>
+      <div className="inline-flex items-center gap-8 my-1">
         <div className="inline-flex gap-1 items-center">
           <BiTimeFive className="text-xl" />
           <Accent className="text-lg">{post.readingTime}</Accent>
@@ -27,41 +67,10 @@ const PostContent: FC<{
       </div>
       <div
         className={clsx(
-          'w-full h-1 my-4  rounded-lg',
+          'w-full h-[2px] my-1  rounded-lg',
           theme === 'light' ? 'bg-gray-700' : 'bg-gray-200'
         )}
       />
-
-      <section
-        className={clsx(
-          'flex flex-col-reverse justify-between',
-          'md:flex-row '
-        )}
-      >
-        <article
-          className={clsx(
-            'prose-sm',
-            // 'prose-invert',
-            'pt-10 pb-8 pr-2',
-            'prose-blockquote:border-4 prose-blockquote:border-transparent',
-            'prose-blockquote:border-l-primary-500 prose-blockquote:py-0',
-            'prose-code:text-primary-500',
-            'prose-code:text-sm',
-            'prose-li:list-disc',
-            'prose-a:text-primary-500',
-            'md:w-9/12',
-            theme === 'light' && 'text-dark'
-          )}
-        >
-          <MDXContent components={MDXComponents} />
-        </article>
-
-        <aside className="py-7 lg:sticky top-16 self-start md:w-3/12">
-          <TableOfContents post={post} />
-        </aside>
-      </section>
-    </main>
+    </>
   );
-};
-
-export default PostContent;
+}
