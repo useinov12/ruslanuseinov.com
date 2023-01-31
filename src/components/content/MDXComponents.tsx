51e4f785ddf9cb4befc706cfe321929d01cad022
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-31 18:18:07 -0500

    ⚙️ Reafctor: unused component removed from MdxComponents

--- src/components/content/MDXComponents.tsx
+++ src/components/content/MDXComponents.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import Footer from '../PageLayout/sharedUI/Footer';
 import TechIcons from './TechIcons';
 
 const MDXComponents = {
   Image,
   Head,
-  Footer,
   a:(props:any)=> <a href={props.href} target='_blank' rel="noreferrer" >{props.children}</a>,
   TechIcons
 };
