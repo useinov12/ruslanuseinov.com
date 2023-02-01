f324f593859f21fbc3cab351eb3e123441bccec3
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-31 19:14:37 -0500

    ⚙️ Refactor: Homepage 1. Layout fixed 2. Sub components created 3. Minor style changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Layout from '../components/layout/Layout';
 import clsx from 'clsx';
-import Accent from '../components/Accent';
 import Button from '../components/Button';
 import UnstyledLink from '../components/UnstyledLink';
 
@@ -10,89 +9,101 @@ import Link from 'next/link';
 import useLoaded from 'src/hooks/useLoaded';
 import { useTheme } from 'src/context/ThemeProvider';
 import Seo from 'src/components/Seo';
-import { useRouter } from 'next/router';
 
 export default function HomePage() {
-  const { pathname } = useRouter();
   const isLoaded = useLoaded();
-  const { theme } = useTheme();
   return (
-    <Layout>
+    <>
       <Seo
-        
         title="Online Portfolio"
-        description='An online portfolio and blog by Ruslan Useinov'
+        description="An online portfolio and blog by Ruslan Useinov"
         imageUrl={'/assets/banners/pw_banner.png'}
       />
-      <main
-        className={clsx(
-          'max-w-screen-lg',
-          'flex flex-col justify-center',
-          'w-full h-full',
-          isLoaded && 'fade-in-start'
-        )}
-      >
-        <section
-          className="p-2 lg:p-0 relative h-[95vh] flex flex-col justify-center"
-          data-fade="1"
+
+      <Layout>
+        <main
+          className={clsx(
+            'mx-auto',
+            'max-w-screen-lg',
+            'flex flex-col justify-center',
+            'w-full h-full',
+            isLoaded && 'fade-in-start'
+          )}
         >
-          <h1 className="text-4xl md:text-6xl tracking-tight font-extrabold">
-            {theme === 'light' ? (
-              'Front End Developer'
-            ) : (
-              <Accent>Front End Developer</Accent>
-            )}
-          </h1>
-          <p className="text-xl md:text-xl my-3 cursor-default" data-fade="2">
-            I love to create beautiful and performant web with delightful user
-            experiences
-          </p>
+          <MainHero />
+        </main>
+      </Layout>
+    </>
+  );
+}
 
-          <div
+function MainHero() {
+  const { theme } = useTheme();
+  return (
+    <section
+      className="p-2 lg:p-0 relative h-[95vh] flex flex-col justify-center"
+      data-fade="1"
+    >
+      <h1 className="text-4xl md:text-6xl tracking-tight font-extrabold">
+        Front End Developer
+      </h1>
+      <p className="text-xl md:text-xl my-3 cursor-default" data-fade="2">
+        I love to create beautiful and performant web with delightful user
+        experiences
+      </p>
+
+      <div
+        className={clsx(
+          'w-full h-[2px] my-4  rounded-lg',
+          theme === 'light' ? 'bg-gray-700' : 'bg-gray-200'
+        )}
+        data-fade="3"
+      />
+
+      <div className="inline-flex gap-3" data-fade="4">
+        <Link href="/blog">
+          <Button className="text-lg">Read Blog</Button>
+        </Link>
+        <Link href="/about">
+          <Button className="text-lg">About</Button>
+        </Link>
+      </div>
+
+      <PersonalLinks />
+
+      <div data-fade="6">
+        <Polkadots className="hidden md:block md:absolute -bottom-36 right-0 -z-10 w-2/5" />
+      </div>
+    </section>
+  );
+}
+
+function PersonalLinks() {
+  const { theme } = useTheme();
+
+  return (
+    <ul className="flex flex-wrap w-full gap-5 my-4 z-10" data-fade="5">
+      {linkList.map(({ text, path, Icon }) => (
+        <li key={text} className="list-none">
+          <UnstyledLink
+            href={path}
             className={clsx(
-              'w-full h-[2px] my-4  rounded-lg',
-              theme === 'light' ? 'bg-gray-700' : 'bg-gray-200'
+              'inline-flex items-center gap-1 ',
+              theme === 'light'
+                ? 'text-gray-900 hover:text-gray-800'
+                : 'text-gray-300 hover:text-white',
+              'focus:outline-none',
+              'transition-colors',
+              'cursor-alias',
+              'drop-shadow hover:drop-shadow-xl'
             )}
-            data-fade="3"
-          />
-
-          <div className="inline-flex gap-3" data-fade="4">
-            <Link href="/blog">
-              <Button className="text-lg">Read Blog</Button>
-            </Link>
-            <Link href="/about">
-              <Button className="text-lg">About</Button>
-            </Link>
-          </div>
-
-          <div className="flex flex-wrap w-full gap-5 my-4 z-10" data-fade="5">
-            {linkList.map(({ text, path, Icon }) => (
-              <li key={text} className="list-none">
-                <UnstyledLink
-                  href={path}
-                  className={clsx(
-                    'inline-flex items-center gap-1 ',
-                    theme === 'light'
-                      ? 'text-gray-900 hover:text-gray-800'
-                      : 'text-gray-300 hover:text-white',
-                    'focus:outline-none',
-                    'transition-colors',
-                    'cursor-alias',
-                    'drop-shadow hover:drop-shadow-xl'
-                  )}
-                >
-                  <Icon className="text-2xl" />
-                  <span className="text-xl">{text}</span>
-                </UnstyledLink>
-              </li>
-            ))}
-          </div>
-          <div data-fade="6">
-            <Polkadots className="hidden md:block md:absolute -bottom-36 right-0 -z-10 w-2/5" />
-          </div>
-        </section>
-      </main>
-    </Layout>
+          >
+            <Icon className="text-2xl" />
+            <span className="text-xl">{text}</span>
+          </UnstyledLink>
+        </li>
+      ))}
+    </ul>
   );
 }
 
