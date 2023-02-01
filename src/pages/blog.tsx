322dc386c5b230c0e3fd9a9c28a601fab0af16f4
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-31 19:43:46 -0500

    ⚙️ Refactor: BlogPage 1. Layout fixed 2. Sub components created

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,8 +1,3 @@
-import {
-  type GetStaticProps,
-  type InferGetStaticPropsType,
-  NextPage,
-} from 'next';
 import Link from 'next/link';
 import Layout from 'src/components/layout/Layout';
 import PostCard from 'src/components/content/PostCard';
@@ -10,10 +5,9 @@ import { allPosts, type Post } from 'contentlayer/generated';
 import clsx from 'clsx';
 import useLoaded from 'src/hooks/useLoaded';
 
-import { NextSeo } from 'next-seo';
 import { useTheme } from 'src/context/ThemeProvider';
 import Seo from 'src/components/Seo';
-import { useRouter } from 'next/router';
+import PageHeader from 'src/components/PageHeader';
 
 export async function getStaticProps() {
   const posts = allPosts.filter((post) =>
@@ -26,38 +20,26 @@ export async function getStaticProps() {
   };
 }
 
-const BlogPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
-  posts,
-}) => {
-  const { pathname } = useRouter();
+export default function BlogPage({ posts }: { posts: Post[] }) {
   const isLoaded = useLoaded();
   const { theme } = useTheme();
   return (
     <Layout>
       <Seo
-        
         description={'Blog by Ruslan Useinov'}
         imageUrl={'/assets/banners/blog_banner.png'}
       />
-      <main className={clsx(isLoaded && 'fade-in-start', 'h-screen')}>
-        <h1
-          className={clsx(
-            'my-4 text-primary-500"',
-            theme === 'light' ? 'text-gray-800' : 'text-primary-500"'
-          )}
-          data-fade="1"
-        >
-          Blog
-        </h1>
-        <h6 className="font-semibold text-xl" data-fade="2">
-          For patterns, tutorials and my setups
-        </h6>
-        <div
-          className={clsx(
-            'w-full h-[1px] mb-8 mt-4 rounded-lg',
-            theme === 'light' ? 'bg-gray-800' : 'bg-gray-300'
-          )}
-          data-fade="3"
+      <main
+        className={clsx(
+          'mx-auto',
+          'max-w-screen-lg',
+          'h-screen',
+          isLoaded && 'fade-in-start'
+        )}
+      >
+        <PageHeader
+          title="Blog"
+          heading="For patterns, tutorials and my setups"
         />
         <ul className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3" data-fade="4">
           {posts.map((postSummary) => {
@@ -77,5 +59,4 @@ const BlogPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
       </main>
     </Layout>
   );
-};
-export default BlogPage;
+}
