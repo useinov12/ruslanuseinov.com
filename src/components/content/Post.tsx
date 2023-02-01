fe8c825dddf59ea4a1c9151518140a919dd187cc
Ruslan Useinov <r.useinov23@gmail.com>
2023-02-01 17:15:13 -0500

    ✨ Style: Post  styles update

--- src/components/content/Post.tsx
+++ src/components/content/Post.tsx
@@ -1,8 +1,4 @@
 import clsx from 'clsx';
-import dayjs from 'dayjs';
-import { BiTimeFive } from 'react-icons/bi';
-import Accent from '../Accent';
-
 import MDXComponents from '../content/MDXComponents';
 import TableOfContents from 'src/components/TableOfContent';
 import { useTheme } from 'src/context/ThemeProvider';
@@ -28,11 +24,8 @@ export default function Post({ post }: { post: Post }) {
         <aside data-fade="3" className={clsx('col-span-3 w-3/12')}>
           <TableOfContents post={post} className={clsx('sticky top-44')} />
         </aside>
-        <main
-          data-fade="2"
-          className={clsx('h-full w-9/12', 'space-y-56  p-4')}
-        >
-          <Article post={post} className="md:w-9/12" />
+        <main data-fade="2" className="h-full w-9/12 space-y-56  p-4 ">
+          <Article post={post} />
         </main>
       </div>
     </div>
@@ -47,24 +40,26 @@ function Article({ post, className }: { post: Post; className?: string }) {
     <article
       className={clsx(
         'prose-lg',
-        'pb-8 pr-6',
         'prose-blockquote:border-4 prose-blockquote:border-transparent',
         'prose-blockquote:border-l-primary-500 prose-blockquote:py-0',
         'prose-code:text-primary-500',
         'prose-li:list-disc prose-li:p-0 prose-li:m-0',
         'prose-a:text-primary-500',
-        // 'prose-p:font-semibold',
-        'prose-headings:my-2',
-        'prose-p:my-0',
         'prose-h1:my-4',
+        'prose-h2:my-3',
+        'prose-h3:my-2',
+        'prose-h4:my-1',
         'prose-ul:m-0',
+        'prose-p:tracking-tighter',
         theme === 'light' && 'text-dark',
         'flex flex-col',
-        // 'max-w-screen-md',
-        className
+        'md:w-4/5',
+        'font-poppins',
+        className,
+        'font-medium'
       )}
     >
-      <ArticleHeader post={post} />
+      <h2 className="text-center font-inter">{post.title}</h2>
       <div className="py-5">
         {post.coverImage && (
           <Image
@@ -80,29 +75,5 @@ function Article({ post, className }: { post: Post; className?: string }) {
         <MDXContent components={MDXComponents} />
       </div>
     </article>
-  );
-}
-
-function ArticleHeader({ post }: { post: Post }) {
-  const { theme } = useTheme();
-  return (
-    <>
-      <h4 className="text-2xl my-1">{post.title}</h4>
-      <div className="inline-flex items-center gap-8 my-1">
-        <div className="inline-flex gap-1 items-center">
-          <BiTimeFive className="text-xl" />
-          <Accent className="text-lg">{post.readingTime}</Accent>
-        </div>
-        <span className="text-lg">
-          {dayjs(post.publishedAt).format('MMMM D, YYYY')}
-        </span>
-      </div>
-      <div
-        className={clsx(
-          'w-full h-[2px] my-1  rounded-lg',
-          theme === 'light' ? 'bg-gray-700' : 'bg-gray-200'
-        )}
-      />
-    </>
   );
 }
