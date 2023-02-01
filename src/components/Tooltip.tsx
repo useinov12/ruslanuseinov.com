7b688ceda390d137a31deed59731572307e4b269
Ruslan Useinov <r.useinov23@gmail.com>
2023-01-31 20:44:00 -0500

    <0001fab2> Deployment Fix: temporary removed tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,16 +1,6 @@
-//@ts-nocheck
-
 import clsx from 'clsx';
 import * as React from 'react';
-import { Tooltip as ReactTooltip } from 'react-tippy';
-
-// type TooltipTextProps = {
-//   content?: React.ReactNode;
-//   children?: React.ReactNode;
-//   className?: string;
-//   spanClassName?: string;
-//   withUnderline?: boolean;
-// }  & TooltipProps & Omit<React.ComponentPropsWithoutRef<'div'>, 'children' | 'className'>;
+import { Tooltip as TippyTooltip, TooltipProps } from 'react-tippy';
 
 type TooltipTextProps = {
   content?: React.ReactNode;
@@ -18,45 +8,42 @@ type TooltipTextProps = {
   className?: string;
   spanClassName?: string;
   withUnderline?: boolean;
-} & any;
+} & TooltipProps &
+  Omit<React.ComponentPropsWithoutRef<'div'>, 'children' | 'className'>;
 
-const Tooltip = ({
+export default function Tooltip({
   content,
   children,
   className,
   spanClassName,
   withUnderline = false,
-  ...rest
-}: TooltipTextProps) => {
-  return (
-    <ReactTooltip
-      trigger="mouseenter"
-      interactive
-      html={
-        <div
-          className={clsx(
-            className,
-            'inline-block rounded-md bg-white p-2 text-gray-600 shadow-md dark:bg-dark dark:text-gray-200',
-            'border dark:border-gray-600 '
-          )}
-        >
-          {content}
-        </div>
-      }
-      {...rest}
-    >
-      {withUnderline ? (
-        <span
-          className={clsx(spanClassName, 'underline')}
-          style={{ textDecorationStyle: 'dotted' }}
-        >
-          {children}
-        </span>
-      ) : (
-        <>{children}</>
-      )}
-    </ReactTooltip>
+}: TooltipTextProps) {
+  return ( <></>
+    // <TippyTooltip
+    //   // trigger="mouseenter"
+    //   interactive
+    //   html={
+    //     <div
+    //       className={clsx(
+    //         className,
+    //         'inline-block rounded-md bg-white p-2 text-gray-600 shadow-md dark:bg-dark dark:text-gray-200',
+    //         'border dark:border-gray-600 '
+    //       )}
+    //     >
+    //       {content}
+    //     </div>
+    //   }
+    // >
+    //   {withUnderline ? (
+    //     <span
+    //       className={clsx(spanClassName, 'underline')}
+    //       style={{ textDecorationStyle: 'dotted' }}
+    //     >
+    //       {children}
+    //     </span>
+    //   ) : (
+    //     <>{children}</>
+    //   )}
+    // </TippyTooltip>
   );
-};
-
-export default Tooltip;
+}
