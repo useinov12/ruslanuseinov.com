304f101ce9b9761f5d2c425dc7482f45f6c5b45d
Ruslan Useinov <r.useinov23@gmail.com>
2023-02-01 17:15:22 -0500

    ✨ Style: PostCard  styles update

--- src/components/content/PostCard.tsx
+++ src/components/content/PostCard.tsx
@@ -20,29 +20,23 @@ const PostCard: FC<{
         'transition duration-25',
         'hover:border-primary-500',
         'flex flex-col justify-between',
-        'p-[2px]',
-
         'drop-shadow hover:drop-shadow-xl',
-        'motion-reduce:hover:scale-100 motion-safe:transform-gpu',
         'scale-100 hover:scale-[1.005] active:scale-[0.995] ',
-        theme === 'light' ? 'bg-dark/70' : 'bg-white',
-        'hover:bg-gradient-to-tr hover:from-primary-500 hover:via-primary-500 hover:to-primary-400',
-
+        'motion-reduce:hover:scale-100 motion-safe:transform-gpu',
+        'border',
+        theme === 'light' ? 'bg-gray-300/10' : 'bg-gray-300/10',
+        theme === 'light' ? 'border-gray-600' : 'border-white',
         coverImage ? 'min-h-[22rem]' : 'min-h-[10rem]',
         'w-full h-full min-w-[12rem] rounded-md'
       )}
     >
       <div
-        className={clsx(
-          'overflow-hidden',
-          'rounded-md w-full h-full relative',
-          theme === 'light' ? 'bg-gray-100' : 'bg-dark'
-        )}
+        className={clsx('overflow-hidden', 'rounded-md w-full h-full relative')}
       >
         {coverImage && (
           <div
             className={clsx(
-              'w-full h-1/2 border-b-2 border-b-white',
+              'w-full h-1/2 border-b border-b-white',
               'group-hover:border-b-primary-500',
               'overflow-hidden',
               'relative'
@@ -55,7 +49,7 @@ const PostCard: FC<{
               objectFit="cover"
               className={clsx(
                 'scale-100 motion-reduce:hover:scale-100 ',
-                'group-hover:scale-[1.05]',
+                'group-hover:scale-[1.015]',
                 'transition duration-75',
                 'drop-shadow'
               )}
