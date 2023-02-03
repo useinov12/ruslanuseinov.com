22375374317809480941302ce93df8af5819008e
Ruslan Useinov <r.useinov23@gmail.com>
2023-02-03 18:41:25 -0500

    style: postcard  styles updated

--- src/components/content/PostCard.tsx
+++ src/components/content/PostCard.tsx
@@ -1,45 +1,34 @@
 import { FC } from 'react';
 import clsx from 'clsx';
-import dayjs from 'dayjs';
 import { type Post } from 'contentlayer/generated';
 import Image from 'next/image';
 import { useTheme } from 'src/context/ThemeProvider';
+import { BsArrowRightShort } from 'react-icons/bs';
 
 const PostCard: FC<{
   postSummary: Post;
 }> = ({
   postSummary: { title, readingTime, description, publishedAt, coverImage },
 }) => {
-  const { theme } = useTheme();
   return (
     <div
       className={clsx(
         'group',
+        'py-5',
         'cursor-pointer',
         'overflow-hidden',
         'transition duration-25',
         'hover:border-primary-500',
         'flex flex-col justify-between',
-        'drop-shadow hover:drop-shadow-xl',
-        'scale-100 hover:scale-[1.005] active:scale-[0.995] ',
-        'motion-reduce:hover:scale-100 motion-safe:transform-gpu',
-        'border',
-        theme === 'light' ? 'bg-gray-300/10' : 'bg-gray-300/10',
-        theme === 'light' ? 'border-gray-600' : 'border-white',
-        coverImage ? 'min-h-[22rem]' : 'min-h-[10rem]',
-        'w-full h-full min-w-[12rem] rounded-md'
       )}
     >
-      <div
-        className={clsx('overflow-hidden', 'rounded-md w-full h-full relative')}
-      >
+      <div className={clsx('overflow-hidden', ' w-full h-full relative')}>
         {coverImage && (
           <div
             className={clsx(
-              'w-full h-1/2 border-b border-b-white',
-              'group-hover:border-b-primary-500',
-              'overflow-hidden',
-              'relative'
+              'w-full h-64 ',
+              'rounded-md overflow-hidden',
+              'relative border-2 group-hover:border-primary-500'
             )}
           >
             <Image
@@ -47,26 +36,19 @@ const PostCard: FC<{
               alt={title}
               layout={'fill'}
               objectFit="cover"
-              className={clsx(
-                'scale-100 motion-reduce:hover:scale-100 ',
-                'group-hover:scale-[1.015]',
-                'transition duration-75',
-                'drop-shadow'
-              )}
             />
           </div>
         )}
-        <div className="p-2 flex flex-col h-full justify-between">
+        <div className="px-2 py-5 flex flex-col h-full justify-between">
           <div>
-            <h4 className="font-mono drop-shadow">{title}</h4>
-            <p className="my-2 font-medium text-md text-primary-500">
-              {readingTime}
-            </p>
-            <p className="py-2 drop-shadow">{description}</p>
+            <h4 className="font-poppins group-hover:text-blue-500">{title}</h4>
+            <p className=" font-medium text-md ">{readingTime}</p>
+            <p className="py-1 font-medium tracking-tight w-5/6">{description}</p>
           </div>
-          <p className="text-primary-500 text-bottom self-end">
-            {dayjs(publishedAt).format('MMMM D, YYYY')}
-          </p>
+          <div className="text-bottom self-start inline-flex gap-1 items-center group-hover:text-blue-600">
+            <h4>Read</h4>
+            <BsArrowRightShort className="text-3xl invisible group-hover:visible group-hover:translate-x-1 transition-all duration-100 ease" />
+          </div>
         </div>
       </div>
     </div>
