dd96696f1101507bf171ebdd1a0b92e09da48af8
Ruslan Useinov <r.useinov23@gmail.com>
2023-02-03 19:18:02 -0500

    fix: width layout fixed

--- src/components/layout/sharedUI/Navbar.tsx
+++ src/components/layout/sharedUI/Navbar.tsx
@@ -8,7 +8,7 @@ import { useTheme } from 'src/context/ThemeProvider';
 
 export default function Navbar() {
   return (
-    <div className="sticky top-0 z-50">
+    <div className="sticky top-0 z-50 w-full overflow-hidden">
       <HorizontalBar />
       <Navigation />
     </div>
